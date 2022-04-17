eb35af70ad9968c56dc739fe6804b8a365933674
Sasha Kolosov <SashaKolosov001@yandex.ru>
2022-04-17 23:53:36 +0300

    Realization header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,17 +7,19 @@ class Header extends Components {
                     <div class="Header__menu-logo-text">Bank</div>
                 </div>
                 <div class="Header__menu-links">
-                    <div class="Header__menu-links-home">
-                        <div class="Header__menu-links-home-text">Home</div>
-                    </div>
-                    <div class="Header__menu-links-cards">
-                        <div class="Header__menu-links-cards-text">Cards</div>
-                    </div>
-                    <div class="Header__menu-links-currencies">
-                        <div class="Header__menu-links-currencies-text">Currencies</div>
-                    </div>
-                    <div class="Header__menu-links-settings">
-                        <div class="Header__menu-links-settings-text">Settings</div>
+                    <div class="Header__menu-links-block">
+                        <div class="Header__menu-links-home">
+                            <div class="Header__menu-links-home-text">Home</div>
+                        </div>
+                        <div class="Header__menu-links-cards">
+                            <div class="Header__menu-links-cards-text">Cards</div>
+                        </div>
+                        <div class="Header__menu-links-currencies">
+                            <div class="Header__menu-links-currencies-text">Currencies</div>
+                        </div>
+                        <div class="Header__menu-links-settings">
+                            <div class="Header__menu-links-settings-text">Settings</div>
+                        </div>
                     </div>
                 </div>
             </div>
