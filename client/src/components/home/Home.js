048dd485903f9f59cd044f00aae198a05e30896a
Sasha Kolosov <SashaKolosov001@yandex.ru>
2022-05-29 22:41:32 +0300

    Start working with security

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,26 +1,7 @@
 class Home extends Components {
     render() {
         return (`<div class="Home" id="home" style="display: ${this.blocked}">
-            <div class="Home__title">
-                <span id="ln-home-title"></span>
-            </div>
-            <div class="Home__offers">
-                <div class="Home__offers-offer">
-                    <div class="Home__offers-offer-left">
-                        <div class="Home__offers-offer-left-title">
-                        
-                        </div>
-                        <div class="Home__offers-offer-left-text">
-
-                        </div>
-                    </div>
-                    <div class="Home__offers-offer-right">
-                        <div class="Home__offers-offer-right-img">
-
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <h1>Page in development...</h1>
         </div>`)
     }
 }
