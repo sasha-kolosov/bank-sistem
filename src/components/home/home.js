db7a75f2e612c8fb21fcb62660541cc97761d54b
Sasha Kolosov <sashakolosov001@yandex.ru>
2022-11-30 18:20:34 +0300

    Первый блок на домашней странице

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,7 +1,30 @@
 class Home {
     render() {
         return `<div class="Home" page="home">
-            <h1>Home</h1>
+            <div class="Home__container">
+                <div class="Home__offers">
+                    <div class="Home__offers-offer">
+                        <div class="Home__offers-offer-finance">
+                            <div class="Home__offers-offer-finance-title">
+                                <span>
+                                    Finance system
+                                </span>
+                            </div>
+                            <div class="Home__offers-offer-finance-text">
+                                <span>
+                                    The best system for calculating your finances.
+                                    We give 1000 bonus rubles
+                                </span>
+                            </div>
+                            <div class="Home__offers-offer-finance-button">
+                                <button path="finance">
+                                    Open system
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>`
     }
 }
